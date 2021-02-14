0362a79218c0ccf4a4dd9fec83ae749d038d12ec
Inna <inna.jobmail@gmail.com>
2021-02-14 20:38:08 +0300

    Создает массив объявлений

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const generateRandomInt = (min, max) => {
   if (min >= 0 && min < max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -5,7 +7,6 @@ const generateRandomInt = (min, max) => {
   throw new Error('Ошибка выполнения');
 };
 
-generateRandomInt();
 
 const generateRandomNum = (min, max, decimal) => {
   if (min >= 0 && min < max) {
@@ -15,4 +16,148 @@ const generateRandomNum = (min, max, decimal) => {
   throw new Error('Ошибка выполнения');
 };
 
-generateRandomNum();
+const adsQuantity = 10;
+
+const usersQuantity = {
+  min: 1,
+  max: 8,
+};
+
+const latitude = {
+  min: 35.65000,
+  max: 35.70000,
+  decimal: 5,
+};
+const longitude = {
+  min: 139.70000,
+  max: 139.80000,
+  decimal: 5,
+};
+
+const ACCOMMODATION = ['Palace', 'Flat', 'House', 'Bungalow'];
+const CHECKTIME = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const titleDescription = ['with a beautiful park view', 'by the lake', 'with fifty cats', 'with aquadisko'];
+const photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+// Генерирую объект  author
+
+const createAvatar = (min, max) => {
+  let upToTen = '';
+  if (max < 10) {
+    upToTen = '0';
+  }
+  return `img/avatars/user${upToTen}${generateRandomInt(min, max)}.png`;
+};
+
+//Генерирую объект location
+
+const createLocation = () => {
+  const location = {
+    x: generateRandomNum(latitude.min, latitude.max, latitude.decimal),
+    y: generateRandomNum(longitude.min, longitude.max, longitude.decimal),
+  }
+  return location;
+};
+
+//Генерирую объект offer
+
+const createDataAccommodation = () => {
+  const randomAccomadationIndex = generateRandomInt(0, ACCOMMODATION.length - 1);
+  const randomTitleDescription = generateRandomInt(0, titleDescription.length - 1);
+  return {
+    title: ACCOMMODATION[randomAccomadationIndex] + ' ' + titleDescription[randomTitleDescription],
+    type: ACCOMMODATION[randomAccomadationIndex],
+  }
+};
+
+const createAddress = () => {
+  const locationX = createLocation().x.toString();
+  const locationY = createLocation().y.toString();
+  return locationX + ', ' + locationY;
+};
+
+const generatePrice = (type) => {
+  switch (type) {
+    case 'Palace':
+      return generateRandomInt(10000, 1000000);
+    case 'Flat':
+      return generateRandomInt(1000, 1000000);
+    case 'House':
+      return generateRandomInt(5000, 1000000);
+    case 'Bungalow':
+      return generateRandomInt(0, 1000000);
+  }
+};
+
+const generateRooms = (type) => {
+  switch (type) {
+    case 'Palace':
+      return generateRandomInt(10, 100);
+    case 'Flat':
+      return generateRandomInt(1, 10);
+    case 'House':
+      return generateRandomInt(5, 20);
+    case 'Bungalow':
+      return generateRandomInt(1, 2);
+  }
+};
+
+const generateCheckTime = () => {
+  const randomCheckIndex = generateRandomInt(0, CHECKTIME.length - 1);
+  return CHECKTIME[randomCheckIndex];
+};
+
+const generateFeatures = () => {
+  const randomFeatureQuantity = generateRandomInt(1, FEATURES.length);
+  const featuresList = [];
+  for (let i = 0; i < randomFeatureQuantity; i++) {
+    const featureIndex = Math.floor(Math.random() * FEATURES.length);
+    featuresList.includes(FEATURES[featureIndex]) ? featureIndex : featuresList.push(FEATURES[featureIndex]);
+  }
+  return featuresList;
+};
+
+const generatePhotos = () => {
+  const randomPhotoQuantity = generateRandomInt(1, photos.length);
+  const photoList = [];
+  for (let i = 0; i < randomPhotoQuantity; i++) {
+    const photoIndex = Math.floor(Math.random() * photos.length);
+    photoList.push(photos[photoIndex]);
+  }
+  return photoList;
+};
+
+const generateAds = () => {
+  const ads = [];
+  for (let i = 0; i < adsQuantity; i++) {
+    const dataAccommodation = createDataAccommodation();
+    const ad = {
+      author: {
+        avatar: createAvatar(usersQuantity.min, usersQuantity.max),
+      },
+      offer: {
+        title: dataAccommodation.title,
+        address: createAddress(),
+        price: generatePrice(dataAccommodation.type),
+        type: dataAccommodation.type,
+        rooms: generateRooms(dataAccommodation.type),
+        guests: function() {
+          return this.rooms * 2;
+        },
+        checkin: generateCheckTime(),
+        checkout: generateCheckTime(),
+        features: generateFeatures(),
+        description: 'Description text for ' + dataAccommodation.type,
+        photos: generatePhotos(),
+      },
+      location: createLocation(),
+    };
+    ads.push(ad);
+  }
+
+  return ads;
+}
+
+generateAds();
+
