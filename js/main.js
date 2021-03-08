d0fa767816c5d8bb0e4c783ad9cf7b6e746e3831
Inna <inna.jobmail@gmail.com>
2021-03-09 00:30:52 +0300

    module5-task2

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 import {generateAds} from './data.js';
 import {createCard} from './card.js';
+import {setTime, timein, timeout} from './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 const adsData = generateAds();
 
 mapCanvas.appendChild(createCard(adsData[0]));
 
-
-
-
-
-
-
-
+setTime(timein);
+setTime(timeout);
